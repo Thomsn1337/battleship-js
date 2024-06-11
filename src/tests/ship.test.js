f69843ff61b1193daf4e36d92a93d972502eb2e3
Thomas Steifler <steiflerthomas@gmail.com>
2024-06-11 17:41:37 +0200

    test: implement test for ship constructor

---
+++ src/tests/ship.test.js
@@ -0,0 +1,11 @@
+import Ship from "../lib/ship";
+
+describe("Test ship class", () => {
+    test("Ship is created with correct length", () => {
+        const ship1 = new Ship(5);
+        const ship2 = new Ship(2);
+
+        expect(ship1.length).toBe(5);
+        expect(ship2.length).toBe(2);
+    });
+});
