2f2de045c21205b1e07d17c5b2fd13ac9ee552b3
Thomas Steifler <steiflerthomas@gmail.com>
2024-06-11 17:51:41 +0200

    feat: add hit method

--- src/lib/ship.js
+++ src/lib/ship.js
@@ -8,6 +8,11 @@ export default class Ship {
      */
     constructor(length) {
         this.#length = length;
+        this.hits = 0;
+    }
+
+    hit() {
+        this.hits += 1;
     }
 
     get length() {
