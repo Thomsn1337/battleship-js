5f5b6d1ce0453650de6896af8e38ccf73b79c3c0
Thomas Steifler <steiflerthomas@gmail.com>
2024-06-11 17:54:32 +0200

    refactor: update hits property to use getter

--- src/lib/ship.js
+++ src/lib/ship.js
@@ -2,20 +2,29 @@ export default class Ship {
     /** @type {number} */
     #length;
 
+    /** @type {number} */
+    #hits;
+
     /**
      * @constructor
      * @param {number} length
      */
     constructor(length) {
         this.#length = length;
-        this.hits = 0;
+        this.#hits = 0;
     }
 
     hit() {
-        this.hits += 1;
+        this.#hits += 1;
     }
 
+    /** @returns {number} */
     get length() {
         return this.#length;
+    }
+
+    /** @returns {number} */
+    get hits() {
+        return this.#hits;
     }
 }
