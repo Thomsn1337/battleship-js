94dec90c10a20c1b9164f3ad9ab717d725122709
Thomas Steifler <steiflerthomas@gmail.com>
2024-06-12 19:07:22 +0200

    feat: implement basic ship placement on board

--- src/lib/gameboard.js
+++ src/lib/gameboard.js
@@ -2,12 +2,42 @@ export default class GameBoard {
     /** @type {Ship[][]} */
     #board;
 
+    /** @type {Ship[]} */
+    #ships;
 
     /** @constructor */
     constructor() {
         this.#board = Array.from({ length: 10 }, () =>
             Array.from({ length: 10 }, () => null),
         );
+
+        this.#ships = [];
+    }
+
+    /**
+     * @param {number[]} coordinates
+     * @param {number} length
+     * @param {string} orientation
+     * @returns {boolean}
+     */
+    placeShip(coordinates, length, orientation) {
+        const [x, y] = coordinates;
+        const ship = new Ship(length);
+        this.#ships.push(ship);
+
+        if (orientation === "h") {
+            for (let i = 0; i < length; i++) {
+                this.#board[x][y + i] = ship;
+            }
+        }
+
+        if (orientation === "v") {
+            for (let i = 0; i < length; i++) {
+                this.#board[x + i][y] = ship;
+            }
+        }
+
+        return true;
     }
 
     get board() {
