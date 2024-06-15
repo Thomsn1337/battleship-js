a6158afe23bf923b34df7139ebc8cc3720884df5
Thomas Steifler <steiflerthomas@gmail.com>
2024-06-16 00:42:32 +0200

    feat: place ships randomly on page load

--- src/index.js
+++ src/index.js
@@ -1,1 +1,28 @@
 import "./style.css";
+import Player from "./lib/player";
+import {
+    createGrid,
+    placeRandom,
+} from "./lib/gameUtils";
+
+const playerGrid = document.querySelector("#player-grid");
+const npcGrid = document.querySelector("#npc-grid");
+
+let humanPlayer;
+let npcPlayer;
+
+function placeShips() {
+    humanPlayer = new Player("human");
+    npcPlayer = new Player("computer");
+
+    placeRandom(humanPlayer);
+    placeRandom(npcPlayer);
+
+    playerGrid.innerHTML = "";
+    npcGrid.innerHTML = "";
+
+    createGrid(playerGrid, humanPlayer);
+    createGrid(npcGrid, npcPlayer);
+}
+
+document.addEventListener("DOMContentLoaded", placeShips);
